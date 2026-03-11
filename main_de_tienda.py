f0f64d4c76d57383fc1b23f30520b2f20c4e454b
Isac <isaacalvarez0516@gmail.com>
2026-03-11 13:16:13 -0500

    Finished

--- main_de_tienda.py
+++ main_de_tienda.py
@@ -1,10 +1,10 @@
-from register_sales import register_sales
+print("Hi, Welcome Mall Store")
+from Register_sales import register_sales
 from totals import calculate_totals
-from summary import show_summary
+from Summary import show_summary
 
-def main():
-    sales = register_sales()
-    total = calculate_totals(sales)
-    show_summary(sales, total)
 
-main()
+sales = register_sales()
+total = calculate_totals(sales)
+show_summary(sales, total)
+
